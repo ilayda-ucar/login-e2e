f8ac8f74260a115a7bc66622d949bd936f34c985
ilayda-ucar <148758580+ilayda-ucar@users.noreply.github.com>
2026-02-06 23:30:32 +0300

    feat: improve success page markup

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Success() {
   return (
-    <div>
+    <div data-cy="success-page">
       <h1>Success</h1>
       <p>Giriş başarılı.</p>
     </div>
