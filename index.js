d6341a2bd870f684b4b2b3843faa7085a38c5c89
ilayda-ucar <148758580+ilayda-ucar@users.noreply.github.com>
2026-02-07 00:24:37 +0300

    chore: add project url

---
+++ index.js
@@ -0,0 +1,11 @@
+function cypressTestProjesi() {
+  const checklist = {
+    //github repo adresini gir: Örn: https://github.com/Workintech/hello-world
+    project_url: "https://github.com/ilayda-ucar/login-e2e",
+  };
+
+  return checklist;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = cypressTestProjesi;
